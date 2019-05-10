53b81addeda43e45e8a7ba81dcd71ccbc8e56748
raidencb <reydenn@callboxinc.com>
2019-05-10 13:46:38 +0800

    replace repo name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/gatsby_journey_01/`,
+  pathPrefix: `/cb_uk_01`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
